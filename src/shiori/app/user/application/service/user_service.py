983d3f2d2633df32cdfdc94f97bc81df951129eb
jungyu333 <jungyu3826@gmail.com>
2025-07-06 21:53:42 +0900

    feat : Add create_user in user_service.py

---
+++ src/shiori/app/user/application/service/user_service.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException
+
+from shiori.app.core.database import Transactional
+from shiori.app.user.domain.entity import UserVO
+from shiori.app.user.domain.repository import UserRepository
+
+
+class UserService:
+
+    def __init__(self, user_repo: UserRepository):
+        self._user_repo = user_repo
+
+    @Transactional()
+    async def create_user(self, email: str, password: str) -> int:
+
+        existed_user = await self._user_repo.get_user_by_email(email)
+
+        if existed_user:
+            raise HTTPException(status_code=422, detail="User already exists")
+
+        userVO = UserVO(email=email, password=password, nickname="nickname")
+
+        user_id = await self._user_repo.save(userVO)
+
+        return user_id
