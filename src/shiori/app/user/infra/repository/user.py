a2b19d749295939547cc66ce4ea317fe488e7e89
jungyu333 <jungyu3826@gmail.com>
2025-07-06 20:47:43 +0900

    feat : Implement get_user_by_email

---
+++ src/shiori/app/user/infra/repository/user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+
+from shiori.app.core.database import session
+from shiori.app.user.domain.entity.user import User as UserVO
+from shiori.app.user.domain.repository import UserRepository
+from shiori.app.user.infra.model.user import User
+
+
+class UserRepositoryImpl(UserRepository):
+    async def get_user_by_email(self, email: str) -> UserVO | None:
+
+        stmt = await session.execute(select(User).where(User.email == email))
+
+        user = stmt.scalars().first()
+
+        if not user:
+            return None
+        return UserVO.from_model(user)
