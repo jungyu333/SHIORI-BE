6a495c1bb1bafe155704bd86ffa7e8a09b83dc0f
jungyu333 <jungyu3826@gmail.com>
2025-07-07 21:09:44 +0900

    feat : password hashing

--- src/shiori/app/user/application/service/user_service.py
+++ src/shiori/app/user/application/service/user_service.py
@@ -2,12 +2,14 @@ from shiori.app.core.database import Transactional
 from shiori.app.core.exceptions import ValidationException
 from shiori.app.user.domain.entity import UserVO
 from shiori.app.user.domain.repository import UserRepository
+from shiori.app.utils.crypto import Crypto
 
 
 class UserService:
 
     def __init__(self, user_repo: UserRepository):
         self._user_repo = user_repo
+        self._crypto = Crypto()
 
     @Transactional()
     async def create_user(self, email: str, password: str, nickname: str) -> int:
@@ -17,7 +19,7 @@ class UserService:
         if existed_user:
             raise ValidationException(message="이미 존재하는 회원입니다")
 
-        userVO = UserVO(email=email, password=password, nickname=nickname)
+        userVO = UserVO(email=email, password= self._crypto.encrypt(password), nickname=nickname)
 
         user_id = await self._user_repo.save(userVO)
 
