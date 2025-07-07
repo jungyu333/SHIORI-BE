a867e207b2db1cb0153e15940f4f3236d4d017f4
jungyu333 <jungyu3826@gmail.com>
2025-07-08 02:23:37 +0900

    feat : fix sys path

--- migrations/env.py
+++ migrations/env.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/src')
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from logging.config import fileConfig
 
 from alembic import context
--- src/shiori/main.py
+++ src/shiori/main.py
@@ -1,4 +1,10 @@
 import os
+import sys
+
+sys.path.insert(
+    0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+)
+
 
 import click
 import uvicorn
