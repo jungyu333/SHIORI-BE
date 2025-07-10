f88dc83d20cfafedde6f8da18fad6ac77f88516e
jungyu333 <jungyu3826@gmail.com>
2025-07-10 19:20:43 +0900

    feat : Add token exceptions

--- src/shiori/app/core/exceptions/base.py
+++ src/shiori/app/core/exceptions/base.py
@@ -26,3 +26,13 @@ class ServerErrorException(BaseCustomException):
 class ValidationException(BaseCustomException):
     def __init__(self, message: str = "요청값이 올바르지 않습니다", data=None):
         super().__init__(code=422, message=message, data=data)
+
+
+class DecodeTokenException(BaseCustomException):
+    def __init__(self, message: str = "TOKEN__DECODE_ERROR", data=None):
+        super().__init__(code=400, message=message, data=data)
+
+
+class ExpiredTokenException(BaseCustomException):
+    def __init__(self, message: str = "TOKEN__EXPIRE_TOKEN", data=None):
+        super().__init__(code=401, message=message, data=data)
