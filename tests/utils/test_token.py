72d023eac7632a8867670922ef7faa355103776b
jungyu333 <jungyu3826@gmail.com>
2025-07-10 22:13:02 +0900

    feat : Add test_decode

--- tests/utils/test_token.py
+++ tests/utils/test_token.py
@@ -28,3 +28,16 @@ def test_encode():
     )
     assert decoded_token["user_id"] == 1
     assert decoded_token["is_admin"] == True
+
+
+@pytest.mark.asyncio
+def test_decode():
+    # Given
+    token = jwt.encode({"user_id": 1, 'is_admin': True}, config.JWT_SECRET_KEY, config.JWT_ALGORITHM)
+
+    # When
+    sut = TokenHelper.decode(token=token)
+
+    # Then
+    assert sut["user_id"] == 1
+    assert sut["is_admin"] == True
