e42838b6d7d2eabad4e9baaee2e80b31eaf83746
jungyu333 <jungyu3826@gmail.com>
2025-07-11 22:08:06 +0900

    feat : Add Description to SignUpResponse

--- src/shiori/app/user/interface/dto/response/__init__.py
+++ src/shiori/app/user/interface/dto/response/__init__.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class SignUpResponse(BaseModel):
-    user_id: int
+    user_id: int = Field(..., description="User Id")
 
 
 class LogInResponse(BaseModel):
