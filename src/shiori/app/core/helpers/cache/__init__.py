192b8ed10e45132f86d83bc43d624043b16686bb
jungyu333 <jungyu3826@gmail.com>
2025-07-13 14:24:28 +0900

    feat : Add Cache Tag

--- src/shiori/app/core/helpers/cache/__init__.py
+++ src/shiori/app/core/helpers/cache/__init__.py
@@ -1,8 +1,9 @@
+from .cache_tag import CacheTag
 from .custom_key_maker import CustomKeyMaker
 from .redis_backend import RedisBackend
-
 
 __all__ = [
     "CustomKeyMaker",
     "RedisBackend",
+    "CacheTag",
 ]
