2f594c81137f3328bcf54a72b363a5be8a17dc2f
jungyu <96876293+jungyu333@users.noreply.github.com>
2025-07-13 13:18:43 +0900

    [FEAT] Setting Redis (#6)

--- src/shiori/app/core/config.py
+++ src/shiori/app/core/config.py
@@ -17,6 +17,9 @@ class BaseConfig(BaseSettings):
     APP_PORT: int = 8080
     WRITER_DB_URL: str = "mysql+aiomysql://fastapi:fastapi@localhost:33306/shiori"
     READER_DB_URL: str = "mysql+aiomysql://fastapi:fastapi@localhost:33306/shiori"
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
     JWT_SECRET_KEY: str = "shiori"
     JWT_ALGORITHM: str = "HS256"
 
