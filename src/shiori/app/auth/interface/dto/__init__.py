2a1cba3c29ed8b6b24ceeefea743a75afe3c721c
jungyu333 <jungyu3826@gmail.com>
2025-07-14 23:05:30 +0900

    feat : Add Refresh Request DTO

--- src/shiori/app/auth/interface/dto/__init__.py
+++ src/shiori/app/auth/interface/dto/__init__.py
@@ -1,3 +1,8 @@
+from .request import RefreshRequest
 from .response import RefreshTokenResponse, RefreshResponse
 
-__all__ = ["RefreshTokenResponse", "RefreshResponse"]
+__all__ = [
+    "RefreshTokenResponse",
+    "RefreshResponse",
+    "RefreshRequest",
+]
