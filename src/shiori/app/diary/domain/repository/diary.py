e8c4044b5184631044cb0ea39c5505e7cc5cc01f
jungyu333 <jungyu3826@gmail.com>
2025-07-27 15:26:34 +0900

    feat : Implement DiaryRepository

---
+++ src/shiori/app/diary/domain/repository/diary.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+from shiori.app.diary.domain.entity import DiaryVO
+
+
+class DiaryRepository(ABC):
+
+    @abstractmethod
+    async def save_diary(self, diary: DiaryVO) -> str:
+        pass
