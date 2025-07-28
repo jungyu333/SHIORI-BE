b4d4ba079fede74d7eb0fd12d63343327fb84fde
jungyu333 <jungyu3826@gmail.com>
2025-07-28 23:37:29 +0900

    feat : Add Session Params to DiaryRepository

--- src/shiori/app/diary/infra/repository/diary.py
+++ src/shiori/app/diary/infra/repository/diary.py
@@ -1,3 +1,4 @@
+from shiori.app.core.database.mongo_session import get_mongo_session
 from shiori.app.diary.domain.entity import DiaryVO
 from shiori.app.diary.domain.repository import DiaryRepository
 from shiori.app.diary.infra.model import DiaryDocument
@@ -6,17 +7,20 @@ from shiori.app.diary.infra.model import DiaryDocument
 class DiaryRepositoryImpl(DiaryRepository):
     async def save_diary(self, diary: DiaryVO) -> str:
 
+        session = get_mongo_session()
+
         existed_document = await DiaryDocument.find_one(
             DiaryDocument.user_id == diary.user_id,
             DiaryDocument.diary_meta_id == diary.diary_meta_id,
+            session=session,
         )
 
         diary_document = diary.to_model()
 
         if existed_document:
             diary_document.id = existed_document.id
-            await diary_document.replace()
+            await diary_document.replace(session=session)
         else:
-            await diary_document.insert()
+            await diary_document.insert(session=session)
 
         return str(diary_document.id)
