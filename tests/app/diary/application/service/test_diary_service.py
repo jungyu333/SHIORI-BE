460347d851f8eb38d06fd3fe0d4b3d2b4b131d39
jungyu333 <jungyu3826@gmail.com>
2025-08-04 21:04:28 +0900

    test : add test for save_diary_meta service method

--- tests/app/diary/application/service/test_diary_service.py
+++ tests/app/diary/application/service/test_diary_service.py
@@ -8,9 +8,9 @@ from shiori.app.diary.domain.repository import DiaryRepository, DiaryMetaReposit
 from shiori.app.diary.infra.model import ProseMirror
 
 diary_repository_mock = AsyncMock(spec=DiaryRepository)
-diary_meta_mock = AsyncMock(spec=DiaryMetaRepository)
+diary_meta_repository_mock = AsyncMock(spec=DiaryMetaRepository)
 diary_service = DiaryService(
-    diary_repo=diary_repository_mock, diary_meta_repo=diary_meta_mock
+    diary_repo=diary_repository_mock, diary_meta_repo=diary_meta_repository_mock
 )
 
 
@@ -80,3 +80,26 @@ async def test_save_diary_raises_exception():
         await diary_service.save_diary(
             user_id=user_id, diary_meta_id=diary_meta_id, content=content, date=date
         )
+
+
+@pytest.mark.asyncio
+async def test_save_diary_meta():
+    # Given
+    user_id = 1
+    date = "20250728"
+    title = "dummy_title"
+
+    diary_meta_repository_mock.save_diary_meta.return_value = None
+
+    # When
+
+    response = await diary_service.save_diary_meta(
+        user_id=user_id,
+        date=date,
+        title=title,
+    )
+
+    # Then
+
+    assert diary_meta_repository_mock.save_diary_meta.call_count == 1
+    assert response is None
