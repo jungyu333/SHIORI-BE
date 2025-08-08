2adae7894d2f25f9f28d1bd313bbc1598bc50474
jungyu333 <jungyu3826@gmail.com>
2025-08-08 21:28:01 +0900

    feat: implement GET /{date} endpoint to retrieve diary content

--- src/shiori/app/diary/interface/dto/response/__init__.py
+++ src/shiori/app/diary/interface/dto/response/__init__.py
@@ -1,11 +1,9 @@
 from pydantic import BaseModel, Field
 
-from shiori.app.diary.infra.model import ProseMirror
-
 
 class UpsertDiaryResponse(BaseModel):
     id: str = Field(..., description="Diary Id")
 
 
 class GetDiaryResponse(BaseModel):
-    content: ProseMirror = Field(..., description="Diary Content")
+    content: dict = Field(..., description="Diary Content")
--- src/shiori/app/diary/interface/router/diary.py
+++ src/shiori/app/diary/interface/router/diary.py
@@ -4,8 +4,12 @@ from fastapi import APIRouter, Depends, Path, Request
 from shiori.app.container import Container
 from shiori.app.core.dependencies import IsAuthenticated, PermissionDependency
 from shiori.app.core.response import StandardResponse
-from shiori.app.diary.application.usecase import UpsertDiary
-from shiori.app.diary.interface.dto import UpsertDiaryRequest, UpsertDiaryResponse
+from shiori.app.diary.application.usecase import UpsertDiary, GetDiary
+from shiori.app.diary.interface.dto import (
+    UpsertDiaryRequest,
+    UpsertDiaryResponse,
+    GetDiaryResponse,
+)
 
 router = APIRouter()
 
@@ -42,5 +46,34 @@ async def upsert_diary(
     return {
         "code": 201 if is_created else 200,
         "message": "저장 되었어요!",
+        "data": response,
+    }
+
+
+@router.get(
+    "/{date}",
+    response_model=StandardResponse[GetDiaryResponse],
+    dependencies=[Depends(PermissionDependency([IsAuthenticated]))],
+)
+@inject
+async def get_diary(
+    request: Request,
+    date: str = Path(..., description="일기 작성 날짜 (예: 20250805)"),
+    use_case: GetDiary = Depends(Provide[Container.get_diary]),
+):
+
+    user_id = request.user.id
+    content = await use_case.execute(
+        date=date,
+        user_id=user_id,
+    )
+
+    response = GetDiaryResponse(
+        content=content,
+    )
+
+    return {
+        "code": 200,
+        "message": "",
         "data": response,
     }
