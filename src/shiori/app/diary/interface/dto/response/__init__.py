2ee23b4329f15b17107d051c6800e12892a6f7b6
jungyu333 <jungyu3826@gmail.com>
2025-08-08 21:22:22 +0900

    feat: add response DTOs for get diary API

--- src/shiori/app/diary/interface/dto/response/__init__.py
+++ src/shiori/app/diary/interface/dto/response/__init__.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, Field
 
+from shiori.app.diary.infra.model import ProseMirror
+
 
 class UpsertDiaryResponse(BaseModel):
     id: str = Field(..., description="Diary Id")
+
+
+class GetDiaryResponse(BaseModel):
+    content: ProseMirror = Field(..., description="Diary Content")
