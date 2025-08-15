86455c219ea9fe564bcd65e22077815b0502c5c2
jungyu333 <jungyu3826@gmail.com>
2025-08-15 16:51:02 +0900

    test: add integration test for GetWeekDiaryMeta API with diary data setup

--- tests/app/diary/interface/router/test_diary.py
+++ tests/app/diary/interface/router/test_diary.py
@@ -7,6 +7,11 @@ from shiori.app.utils.helpers import TokenHelper
 BASE_URL = "http://test"
 
 
+@pytest.fixture
+def access_token_mock():
+    return TokenHelper.encode({"user_id": 1, "is_admin": False})
+
+
 @pytest.mark.mongo
 @pytest.mark.asyncio
 async def test_upsert_diary():
@@ -201,3 +206,41 @@ async def test_get_diary():
     assert response.json().get("code") == 200
     assert response.json().get("message") == ""
     assert response.json().get("data").get("content") == content
+
+
+@pytest.mark.mongo
+@pytest.mark.asyncio
+async def test_get_week_diary_meta(access_token_mock):
+    # Given
+    access_token = access_token_mock
+
+    start_date = "20250810"
+    end_date = "20250816"
+
+    test_dates = ["20250810", "20250812", "20250814"]
+    content = {
+        "type": "doc",
+        "content": [
+            {
+                "type": "paragraph",
+                "attrs": {"textAlign": "left"},
+                "content": [{"type": "text", "text": "test"}],
+            }
+        ],
+    }
+
+    body = {"content": content, "title": "test_title"}
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    async with AsyncClient(app=app, base_url=BASE_URL, headers=headers) as client:
+        for date in test_dates:
+            await client.post(f"/api/diary/{date}", json=body)
+
+        # When
+        response = await client.get(f"/api/diary?start={start_date}&end={end_date}")
+
+    # Then
+    assert response.json().get("code") == 200
+    assert response.json().get("message") == ""
+    assert len(response.json().get("data")) == len(test_dates)
