35e1ffec22156642204385fc8d8536a4a44ded52
jungyu333 <jungyu3826@gmail.com>
2025-08-15 16:38:38 +0900

    feat: Update GetWeekDiaryMeta API to return plain list of WeekDiaryMeta in response

--- src/shiori/app/diary/interface/dto/response/__init__.py
+++ src/shiori/app/diary/interface/dto/response/__init__.py
@@ -7,3 +7,11 @@ class UpsertDiaryResponse(BaseModel):
 
 class GetDiaryResponse(BaseModel):
     content: dict | None = Field(..., description="Diary Content")
+
+
+class WeekDiaryMeta(BaseModel):
+    date: str
+    title: str
+    summary_status: str
+    is_archived: bool
+    updated_at: str
