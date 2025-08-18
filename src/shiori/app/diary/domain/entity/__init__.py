09ad56ac54e768d82b249965a85ca5063174aa70
jungyu333 <jungyu3826@gmail.com>
2025-08-18 21:49:44 +0900

    chore: export TagVO from domain init

--- src/shiori/app/diary/domain/entity/__init__.py
+++ src/shiori/app/diary/domain/entity/__init__.py
@@ -1,5 +1,7 @@
 from .diary import Diary as DiaryVO, ProseMirror
 from .diary_block import DiaryBlock as DiaryBlockVO
 from .diary_meta import DiaryMeta as DiaryMetaVO, DiaryMeta
+from .tag import Tag as TagVO
 
-__all__ = ["DiaryVO", "ProseMirror", "DiaryMetaVO", "DiaryBlockVO"]
+
+__all__ = ["DiaryVO", "ProseMirror", "DiaryMetaVO", "DiaryBlockVO", "TagVO"]
